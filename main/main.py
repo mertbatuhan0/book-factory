b603b0ac421ed06f2be7282df3c62356d73044df
Batuhan mert Elma <batuhanmertelma20@gmail.com>
2026-05-21 09:05:16 +0000

    done

---
+++ main/main.py
@@ -0,0 +1,30 @@
+import random 
+
+class Book:
+    def __init__(self,title,author):
+        self.title = title
+        self.author = author
+
+
+def GenerateBooks():
+    books = [
+        {"title":"The Little Prince", "author": "Antoine de Saint-Exupéry"},
+        {"title":"Charlotte's Web","author":" E. B. White"}]
+    
+    choosen_book = random.choice(books)
+    return Book(choosen_book["title"], choosen_book["author"])
+
+
+b1 = GenerateBooks()
+print(f"book: {b1.title}, author: {b1.author}")
+
+
+
+        
+
+
+
+
+
+
+ 
